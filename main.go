3ddbf0cc35e948dfca1dcf93d00c84b6778edf72
jenny <jenny@mail.com>
2020-10-31 15:39:08 +0800

    Update: 存json檔 讀json檔 poc

--- main.go
+++ main.go
@@ -1,25 +1,67 @@
 package main
+
 import (
+	"encoding/json"
 	"fmt"
-	// "encoding/json"
+	"io/ioutil"
 	"net/http"
-	// "html/template"
-	// "path"
-	routes "api/router"
+	"time"
 )
 
+type Todo struct {
+	Name string
+}
 
-
-  
 func main() {
 	startServer()
 
-	
 }
 
-func startServer(){
-	fmt.Println("server-start")
-	http.Handle("/",http.FileServer(http.Dir("./f2e")))
-	
+func apiHandle() {
+	http.HandleFunc("/api/getData", getData)
+	http.HandleFunc("/api/saveData", saveData)
+
+}
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	data := readJsonFile()
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(data)
+}
+
+func saveData(w http.ResponseWriter, r *http.Request) {
+	writeJsonFile()
+	w.WriteHeader(200)
+}
+
+type UserDto struct {
+	Id    int    `json: "id"`
+	Name  string `json: "name"`
+	Score int    `json: "score`
+}
+
+func readJsonFile() []UserDto {
+	file, _ := ioutil.ReadFile("./jsonData/user.json")
+	data := []UserDto{}
+	_ = json.Unmarshal([]byte(file), &data)
+	return data
+}
+
+func writeJsonFile() {
+	data := []UserDto{
+		UserDto{
+			Id:    0,
+			Name:  "bigFrog",
+			Score: 100,
+		},
+	}
+	file, _ := json.MarshalIndent(data, "", " ")
+	_ = ioutil.WriteFile("./jsonData/user.json", file, 0644)
+}
+
+func startServer() {
+	fmt.Println("server-start", time.Now())
+	apiHandle()
+	http.Handle("/", http.FileServer(http.Dir("./f2e")))
 	http.ListenAndServe(":8090", nil)
 }
